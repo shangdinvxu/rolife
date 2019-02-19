aafc736b8284c3a2321b25decd0da7789d77487f
shangdinvxu <997399759@qq.com>
2019-02-19 17:42:45 +0800

    Initial commit

---
+++ src/main/java/com/rolife/RolifeApplication.java
@@ -0,0 +1,13 @@
+package com.rolife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RolifeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RolifeApplication.class, args);
+    }
+
+}
